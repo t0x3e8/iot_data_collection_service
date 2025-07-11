49e2fcbb2934cb6bf64ceea567587e4b7f8828d7
t0x3e8 <t0x3e8@gmail.com>
2025-07-11 22:36:59 +0200

    This commit introduces several changes to streamline local development and enhance the testing framework.
       - Simplified Database Configuration: The init.sql script has been removed, and the corresponding Docker volume mount is no longer needed. The application now handles database initialization, simplifying the setup.
       - Development-Friendly Defaults: The default DB_HOST in .env.example is now 127.0.0.1, making it easier to run the application locally without relying on Docker's network.
       - New Dependencies: Added glob and superagent to support enhanced testing and file management capabilities.
       - Cleaned up `package.json`: Removed blank lines from the scripts section.

--- scripts/init.sql
+++
@@ -1,6 +0,0 @@
--- This script will run inside the container on startup
--- Creates a user that can connect from any IP
-CREATE DATABASE IF NOT EXISTS iot_data;
-CREATE USER IF NOT EXISTS 'iot_user'@'%' IDENTIFIED BY 'iot_password';
-GRANT ALL PRIVILEGES ON iot_data.* TO 'iot_user'@'%';
-FLUSH PRIVILEGES;
